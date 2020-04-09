39abf1e92a116f4a6479f5145b2a18024093ac53
Lance_Robot <fanatic_l@163.com>
2020-04-09 09:48:45 +0800

    1.Flex布局，通过主轴和横轴进行定位，YogaKit 基于Flex布局的iOS框架 2.消除警告 	（1）Cocoapods的警告 通过在podfile中加入inhibit_all_warnings!来消除 	（2）Xcode警告，通过Reveal in log中查看警告类型，然后通过如下预编译宏来进行消除 			#pragma clang diagnostic push 			#pragma clang diagnostic ignored "-Wunguarded-availability-new" 			....警告代码 			#pragma clang diagnostic pop

---
+++ iOSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDemo
+//
+//  Created by Ronds on 2020/4/7.
+//  Copyright © 2020 lance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOSDemo
+//
+//  Created by Ronds on 2020/4/7.
+//  Copyright © 2020 lance. All rights reserved.
+//
+
+#import "ViewController.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view.
+}
+
+
+@end
